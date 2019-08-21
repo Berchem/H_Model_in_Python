f8d51932bcc9802d2902af5bab5344ebce924766
Berchem <low118profile@gmail.com>
2019-08-22 00:21:09 +0800

    pilot

--- test/VolumeTest.py
+++ test/VolumeTest.py
@@ -3,6 +3,7 @@
 from Futures.Config import Config
 from Futures.DataUtil import DataUtil
 import os
+import datetime as dt
 
 conf = Config("test-conf.ini")
 
@@ -14,3 +15,15 @@ data_util = DataUtil()
 data = data_util.get_data_from_file(filename, 1)
 print(data.limit(5))
 
+t = dt.datetime.strptime("2017-08-15 09:01:35.17", "%Y-%m-%d %H:%M:%S.%f")
+t_i = dt.datetime.strptime("2017-08-15 09:01:35.7", "%Y-%m-%d %H:%M:%S.%f")
+
+print(t_i - t)
+
+print("%12s: %d" % ("year", t.year))
+print("%12s: %d" % ("month", t.month))
+print("%12s: %d" % ("day", t.day))
+print("%12s: %d" % ("hour", t.hour))
+print("%12s: %d" % ("minute", t.minute))
+print("%12s: %d" % ("second", t.second))
+print("%12s: %d" % ("microsecond", t.microsecond))
